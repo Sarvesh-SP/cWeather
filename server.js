23938ffa610d288f443d20959fda9c812c252852
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2021-05-30 20:12:05 +0530

    added cli features

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+const argv = process.argv.slice(2)
 
 
-geocode('Boston', (error, data) => {
+if (argv[0]) {
+  geocode(`${argv[0]}`, (error, data) => {
 
-  if (error) {
-    return console.log(error);
-  }
-  console.log('Data', data)
-  forecast(data.latitude, data.longitude, (error, data) => {
     if (error) {
       return console.log(error);
     }
-    console.log('Data', data)
+  
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(data.location)
+      console.log(forecastData)
+    })
   })
-})
+} else {
+  console.log('Please enter the location')
+}
 
