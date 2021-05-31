72ff13bd48d386ab6feee074a58f4557cdf895d8
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2021-05-31 07:58:35 +0530

    destructured response

--- utils/forecast.js
+++ utils/forecast.js
@@ -4,13 +4,13 @@ const request = require('request')
 
 const forecast = (lat, lon, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=f2d4ea7c1d1089c0454aaefffbd16908&query=${lat}, ${lon}`
-  request({url: url, json: true} , (error, res) => {
+  request({url: url, json: true} , (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service!', undefined)
-    } else if(res.body.error) {
+    } else if(body.error) {
       callback('Unable to find location', undefined)
     } else {
-      const {temperature, feelslike, weather_descriptions, observation_time} = res.body.current;
+      const {temperature, feelslike, weather_descriptions, observation_time} = body.current;
       let data = {
         temp: temperature,
         feels: feelslike,
