2d78df2fed00a3cc5a14c26b13054033e655385f
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2021-05-31 07:58:57 +0530

    modified location query

--- utils/geocode.js
+++ utils/geocode.js
@@ -3,19 +3,19 @@ const request = require('request')
 const geocode = (address, callback) => {
   const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1Ijoic2FydmVzaHNwIiwiYSI6ImNrcGF2ZDFnNDBydHIyb2xsNW5qZjdiOHMifQ.eN1BZy-iSIqsiqlVSCQ9Pg&limit=1';
 
-  request({url: url, json: true}, (error, res) => {
+  request({url: url, json: true}, (error, {body}) => {
     if (error) {
       callback('Unable to connect to location services.', undefined);
-    } else if (res.body.message) {
+    } else if (body.message) {
       callback("Invalid location")
-    } else if (res.body.features.length === 0) {
+    } else if (body.features.length === 0) {
       callback("Unable to find location. Try another search.")
     }else {
-      const [lon, lat] = res.body.features[0].center;
+      const [lon, lat] = body.features[0].center;
       callback(undefined, {
         lat,
         lon,
-        loc: res.body.features[0].place_name
+        loc: body.features[0].place_name
       })
     }
   })
