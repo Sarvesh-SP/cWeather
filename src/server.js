0f058dddf42e01a4661a5fb6e9feb50b66cfcca5
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2021-06-03 09:00:43 +0530

    Change dir

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const geocode = require('../utils/geocode')
+const forecast = require('../utils/forecast')
 
 const argv = process.argv.slice(2)
 
